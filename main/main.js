e55985a37b5dbe2a697c6951d11c4fcedcf4671c
Withoutmelv <781033189@qq.com>
2019-03-23 15:49:37 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+    var dis=inputs.distance;
+    var tim=inputs.parkTime;
+    var price=0;
+    if(dis<=2){
+        price=6+0.25*tim;
+    }else if(dis>2&&dis<=8){
+        price=6+(dis-2)*0.8+0.25*tim;
+    }else{
+        price=6+6*0.8+0.25*tim+1.2*(dis-8);
+    }
+    price=Math.round(price);
+    return price;
 };
